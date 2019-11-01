18805c885155732b161c6e2c0469139d128ef1fd
Dario Carella <dario.kar@gmail.com>
2019-11-01 14:23:37 +0100

    Setup

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Repo Surf</h1>
+      <p>
+        Styled-components throttle flask cloud. Angular styled-components,
+        lodash styled-components git webvr eslint source stackoverflow python,
+        jquery, django preact shuffle webvr firefox redux jquery ie autoprefixer
+        json, dojo, firebase postcss, uglify boto. Flask cryptography droplet
+        promise firebase isomorphic webvr, classnames backbone python history
+        ec2, ie firefox. Pillow vue, s3. Php polymer. Ajax.
+      </p>
+    </div>
+  );
+}
+
+export default App;
