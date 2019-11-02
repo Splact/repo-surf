4328501301cf9f174d4f80bd5b932ab036bdf21b
Dario Carella <dario.kar@gmail.com>
2019-11-02 17:10:46 +0100

    Setup initial scene

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,8 +1,29 @@
 import React from "react";
+import { Canvas } from "react-three-fiber";
+import * as THREE from "three";
 
+import DummyBlock from "components/DummyBlock";
+import Effects from "components/Effects";
 import useRepo from "hooks/useRepo";
 
 import "./style.scss";
+
+const cameraSettings = {
+  position: [0, 25, 25],
+  near: 0.01,
+  far: 1000,
+  fov: 70
+};
+const backgroundColor = new THREE.Color("#0C0E16");
+const handleCameraInit = camera => {
+  camera.lookAt(0, 0, 0);
+};
+const handleCanvasCreate = ({ gl, camera }) => {
+  // gl.gammaInput = true;
+  // gl.toneMapping = THREE.Uncharted2ToneMapping;
+  gl.setClearColor(backgroundColor);
+  handleCameraInit(camera);
+};
 
 const App = () => {
   const { owner, repo, commits, isLoading, error } = useRepo();
@@ -10,17 +31,19 @@ const App = () => {
   console.log(owner, repo, commits, isLoading, error);
 
   return (
-    <div className="App">
-      <h1>Repo Surf</h1>
-      <p>
-        Styled-components throttle flask cloud. Angular styled-components,
-        lodash styled-components git webvr eslint source stackoverflow python,
-        jquery, django preact shuffle webvr firefox redux jquery ie autoprefixer
-        json, dojo, firebase postcss, uglify boto. Flask cryptography droplet
-        promise firebase isomorphic webvr, classnames backbone python history
-        ec2, ie firefox. Pillow vue, s3. Php polymer. Ajax.
-      </p>
-    </div>
+    <Canvas
+      camera={cameraSettings}
+      onCreated={handleCanvasCreate}
+      pixelRatio={window.devicePixelRatio}
+      updateDefaultCamera
+    >
+      <fog attach="fog" args={[backgroundColor, 100, 700]} />
+      <ambientLight intensity={0.25} />
+
+      <DummyBlock />
+
+      <Effects />
+    </Canvas>
   );
 };
 
---
+++ src/components/Effects/index.js
@@ -0,0 +1,25 @@
+import React, { useRef, useEffect } from "react";
+import { extend, useThree, useFrame } from "react-three-fiber";
+import { EffectComposer } from "three/examples/jsm/postprocessing/EffectComposer";
+import { RenderPass } from "three/examples/jsm/postprocessing/RenderPass";
+import { UnrealBloomPass } from "three/examples/jsm/postprocessing/UnrealBloomPass";
+
+extend({ EffectComposer, RenderPass, UnrealBloomPass });
+
+export default function Effects() {
+  const composer = useRef();
+  const { scene, gl, size, camera } = useThree();
+
+  useEffect(() => void composer.current.setSize(size.width, size.height), [
+    size
+  ]);
+
+  useFrame(() => composer.current.render(), 2);
+
+  return (
+    <effectComposer ref={composer} args={[gl]}>
+      <renderPass attachArray="passes" scene={scene} camera={camera} />
+      <unrealBloomPass attachArray="passes" args={[undefined, 0.5, 1, 0]} />
+    </effectComposer>
+  );
+}
