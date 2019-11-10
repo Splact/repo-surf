5937afc95cdde246aa966eaa1751143dfb577b15
Dario Carella <dario.kar@gmail.com>
2019-11-10 23:18:19 +0100

    Fix scale 0 issue on track

--- src/components/Track/index.js
+++ src/components/Track/index.js
@@ -1,6 +1,5 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef } from "react";
 import { useFrame } from "react-three-fiber";
-import { useSpring, a } from "react-spring/three";
 
 import { useConfig } from "utils/config";
 
@@ -11,45 +10,15 @@ const Track = () => {
   const commitsDistance = useConfig(config => config.commitsDistance);
   const { width, color, emissiveIntensity } = useConfig(config => config.track);
 
-  const [spring, set] = useSpring(() => ({
-    // scale: [1, 1, 0],
-    rotation: [-Math.PI / 2, 0, 0],
-    config: { mass: 10, friction: 20, tension: 150 }
-  }));
-
-  const handlePointerOver = useCallback(
-    e => {
-      // set({ scale: [0.95, 1.05, 1] });
-    },
-    [set]
-  );
-  const handlePointerMove = useCallback(
-    e => {
-      set({ rotation: [-Math.PI / 2, (Math.PI / 24) * e.point.x, 0] });
-    },
-    [set]
-  );
-  const handlePointerOut = useCallback(
-    e => {
-      set({ rotation: [-Math.PI / 2, 0, 0], scale: [1, 1, 1] });
-    },
-    [set]
-  );
-
   useFrame(({ clock }) => {
-    ref.current.scale.y = Math.max(clock.elapsedTime * speed - 1, 0);
+    ref.current.scale.y = Math.max(clock.elapsedTime * speed - 1, 0.000001);
     ref.current.position.z =
       (clock.elapsedTime * speed * commitsDistance - commitsDistance) / 2;
     ref.current.rotation.x = -Math.PI / 2;
   });
 
   return (
-    <mesh
-      ref={ref}
-      onPointerOver={handlePointerOver}
-      onPointerMove={handlePointerMove}
-      onPointerOut={handlePointerOut}
-    >
+    <mesh ref={ref}>
       <planeBufferGeometry attach="geometry" args={[width, commitsDistance]} />
       <meshLambertMaterial
         attach="material"
