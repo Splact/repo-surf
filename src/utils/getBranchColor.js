d85d198170860b7b209db3754b8765138ac0b061
Dario Carella <dario.kar@gmail.com>
2020-01-04 21:31:16 +0100

    Update branch colors

--- src/utils/getBranchColor.js
+++ src/utils/getBranchColor.js
@@ -1,16 +1,16 @@
 import hsb2rgb from "./hsb2rgb";
 import rgb2hex from "./rgb2hex";
-const COLOR_STEP = 0.075;
+const COLOR_STEP = 0.2222;
 const BASE_COLOR = { h: 0.275766017, s: 0.49, b: 0.8 }; // #8ACB67
 
 export default index => {
-  let h = BASE_COLOR.h + COLOR_STEP * (index ? index + 2 : 0);
+  let h = BASE_COLOR.h + COLOR_STEP * index;
   let s = BASE_COLOR.s;
   const b = BASE_COLOR.b;
 
   while (h > 1) {
     h -= 1;
-    s -= 0.2;
+    s -= 0.1;
   }
 
   while (s < 0) {
