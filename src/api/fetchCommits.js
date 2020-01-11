bde76081fa49dd416bb87193f91a9d26f9df9026
Dario Carella <dario.kar@gmail.com>
2020-01-11 19:00:50 +0100

    Load commits via api

--- src/api/fetchCommits.js
+++ src/api/fetchCommits.js
@@ -1,20 +1,7 @@
-import delay from "utils/delay";
-
-// temp solution
 export default async (owner, repo) => {
-  // const commits = await fetch(
-  //   `https://api.github.com/repos/${owner}/${repo}/commits?per_page=100`
-  // );
+  const response = await fetch(
+    `${process.env.REACT_APP_API_BASE_URL}/github/${owner}/${repo}`
+  );
 
-  await delay(1000 * Math.random());
-
-  let commits = [];
-
-  if (owner === "facebook" && repo === "react") {
-    commits = require("data/facebook.react.master.json");
-  } else {
-    commits = require("data/mrdoob.three.js.master.json");
-  }
-
-  return commits;
+  return response.json();
 };
--- src/hooks/useRepo.js
+++ src/hooks/useRepo.js
@@ -32,37 +32,6 @@ export default () => {
       try {
         let cc = await fetchCommits(owner, repo);
 
-        if (cc.length) {
-          cc = cc.map((c, i) => ({
-            sha: c.sha,
-            index: cc.length - i - 1,
-            commit: {
-              authorDate: c.commit.author.date,
-              date: c.commit.committer.date,
-              message: c.commit.message,
-              url: c.commit.url,
-              commentCount: c.commit.comment_count
-            },
-            url: c.url,
-            htmlUrl: c.html_url,
-            author: c.author && {
-              name: c.commit.author.name,
-              username: c.author.login,
-              picture: c.author.avatar_url,
-              url: c.author.url,
-              htmlUrl: c.author.html_url
-            },
-            committer: c.committer && {
-              name: c.commit.committer.name,
-              username: c.committer.login,
-              picture: c.committer.avatar_url,
-              url: c.committer.url,
-              htmlUrl: c.committer.html_url
-            },
-            parents: c.parents.map(p => p.sha)
-          }));
-        }
-
         setState(state => ({ ...state, commits: cc, isLoading: false }));
       } catch (error) {
         setState(state => ({ ...state, isLoading: false, error }));
