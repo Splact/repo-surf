ce774ce6c12f60f48953f5c127831d5dbd7d02df
Dario Carella <dario.kar@gmail.com>
2025-05-17 18:26:11 +0200

    Disable backend requests

--- src/hooks/useRepo.js
+++ src/hooks/useRepo.js
@@ -1,46 +1,51 @@
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
+// import loadingManager, { RESOURCE_TYPE_FETCH } from "utils/loadingManager";
+import splactMorbidoMasterCommits from "data/splact.morbido.master.json";
 
-import loadingManager, { RESOURCE_TYPE_FETCH } from "utils/loadingManager";
+// const getRepoParamsFromPath = () => {
+//   const parts = window.location.pathname.replace(/^\/+|\/+$/g, "").split("/");
+//   if (parts.length < 2) {
+//     return {
+//       owner: undefined,
+//       repo: undefined
+//     };
+//   }
 
-const getRepoParamsFromPath = () => {
-  const parts = window.location.pathname.replace(/^\/+|\/+$/g, "").split("/");
-  if (parts.length < 2) {
-    return {
-      owner: undefined,
-      repo: undefined
-    };
-  }
-
-  return {
-    owner: parts[0],
-    repo: parts[1]
-  };
-};
+//   return {
+//     owner: parts[0],
+//     repo: parts[1]
+//   };
+// };
 
 const useRepo = () => {
-  const [commits, setCommits] = useState([]);
+  // NOTE: backend has been dismantled, so custom repo targeting is turned off
 
-  const { owner = "splact", repo = "morbido" } = getRepoParamsFromPath();
+  // const [commits, setCommits] = useState([]);
+  // const { owner = "splact", repo = "morbido" } = getRepoParamsFromPath();
 
-  useEffect(() => {
-    async function fetch() {
-      const fetchedCommitsResource = loadingManager.registerResource(
-        `${process.env.REACT_APP_API_BASE_URL}/github/${owner}/${repo}`,
-        RESOURCE_TYPE_FETCH
-      );
+  // useEffect(() => {
+  //   async function fetch() {
+  //     const fetchedCommitsResource = loadingManager.registerResource(
+  //       `${process.env.REACT_APP_API_BASE_URL}/github/${owner}/${repo}`,
+  //       RESOURCE_TYPE_FETCH
+  //     );
 
-      // get response as soon as available
-      const fetchedCommits = await fetchedCommitsResource.get();
-      // commits = commits.slice(41, 60);
-      // commits.forEach((c, i) => (c.index = commits.length - i - 1));
+  //     // get response as soon as available
+  //     const fetchedCommits = await fetchedCommitsResource.get();
+  //     // commits = commits.slice(41, 60);
+  //     // commits.forEach((c, i) => (c.index = commits.length - i - 1));
 
-      setCommits(fetchedCommits);
-    }
+  //     setCommits(fetchedCommits);
+  //   }
 
-    if (owner !== "" && repo !== "") {
-      fetch();
-    }
-  }, [owner, repo]);
+  //   if (owner !== "" && repo !== "") {
+  //     fetch();
+  //   }
+  // }, [owner, repo]);
+
+  const owner = "splact";
+  const repo = "morbido";
+  const commits = splactMorbidoMasterCommits;
 
   return { owner, repo, commits };
 };
